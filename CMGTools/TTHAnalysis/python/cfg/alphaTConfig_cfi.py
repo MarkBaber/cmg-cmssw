b1488ea7215c7d4f2b53fd604fec9cca68efc39e
aelwood <adam.elwood09@imperial.ac.uk>
2015-01-13 10:25:27 +0100

    Moved the alphaTConfig to python/cfg so can be used by AlphaTools

--- CMGTools/TTHAnalysis/python/cfg/alphaTConfig_cfi.py
+++ CMGTools/TTHAnalysis/python/cfg/alphaTConfig_cfi.py
@@ -9,14 +9,14 @@ alphaTPSet = cfg.CFG(
 #puRegime = 'PU40bx50',
 puRegime = 'PU20bx25',
 #cutFlow = 'MultiJetEnriched',
-#cutFlow = 'Signal',
+cutFlow = 'Signal',
 #cutFlow = 'SingleMu',
 #cutFlow = 'DoubleMu',
 #cutFlow = 'SinglePhoton',
 #cutFlow = 'SingleEle',
 #cutFlow = 'DoubleEle',
-cutFlow = 'Inclusive',
-test =  2, #does nothing for now (may re add functionality in future)
+#cutFlow = 'Inclusive',
+test =  0, #does nothing for now (may re add functionality in future)
 limitFiles = False,
 )
 
