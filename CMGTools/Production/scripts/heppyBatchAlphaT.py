ff7a26b522b5814004de5c39d4da08d2fc103d8f
aelwood <adam.elwood09@imperial.ac.uk>
2015-02-02 17:06:54 +0000

    Fixed batch scripts for IC

--- CMGTools/Production/scripts/heppyBatchAlphaT.py
+++ CMGTools/Production/scripts/heppyBatchAlphaT.py
@@ -75,7 +75,7 @@ def main(outDir,cfg,cutFlow, tag):
             if location == 'CERN':
                 submissionArgs = "bsub -q 8nh -J "+output+" < batchScript.sh"
             elif location == 'Imperial':
-                submissionArgs = "qsub -q hepshort.q batchScript.sh"
+                submissionArgs = "qsub -q hepshort.q < batchScript.sh"
             else:
                 sys.exit("Don't know where I am, can't submit correctly")
 
