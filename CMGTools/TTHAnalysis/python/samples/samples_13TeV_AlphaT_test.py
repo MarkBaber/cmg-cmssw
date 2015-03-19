da2c8b977ebaa12fb9c7d24358cd5d1b4f7bfc5d
aelwood <adam.elwood09@imperial.ac.uk>
2015-03-19 09:44:27 +0100

    Added samples file for testing with local files. Contains DM currently

---
+++ CMGTools/TTHAnalysis/python/samples/samples_13TeV_AlphaT_test.py
@@ -0,0 +1,572 @@
+import PhysicsTools.HeppyCore.framework.config as cfg
+
+mDM100_mPhi100_A = cfg.MCComponent(
+    name = "mDM100_mPhi100_A",
+    files = [
+        '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM100_mPhi100_fast.root',
+        ],
+    xSection = 37.14
+    )
+
+mDM100_mPhi10_A  = cfg.MCComponent(
+    name = "mDM100_mPhi10_A",
+    files = [
+    '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM100_mPhi10_fast.root',
+    ],
+    xSection = 30.30
+    )
+
+mDM100_mPhi1100_A = cfg.MCComponent(
+    name = "mDM100_mPhi1100_A",
+    files = [
+    '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM100_mPhi1100_fast.root',
+    ],
+    xSection =  3.37
+    )
+
+mDM100_mPhi1300_A = cfg.MCComponent(
+    name = "mDM100_mPhi1300_A", 
+    files = [
+        '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM100_mPhi1300_fast.root', 
+        ],
+    xSection = 1.83
+    )
+
+mDM100_mPhi300_A = cfg.MCComponent(
+    name = "mDM100_mPhi300_A",
+    files = [
+    '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM100_mPhi300_fast.root',
+    ],
+    xSection = 111.61
+    )
+
+mDM100_mPhi500_A = cfg.MCComponent(
+    name = "mDM100_mPhi500_A",
+    files = [
+    '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM100_mPhi500_fast.root',
+    ],
+    xSection = 40.65
+    )
+
+mDM100_mPhi50_A  = cfg.MCComponent(
+    name = "mDM100_mPhi50_A",
+    files = [
+    '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM100_mPhi50_fast.root',
+    ],
+    xSection = 31.74
+    )
+
+mDM100_mPhi700_A = cfg.MCComponent(
+    name = "mDM100_mPhi700_A",
+    files = [
+    '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM100_mPhi700_fast.root',
+    ],
+    xSection = 15.26
+    )
+
+mDM100_mPhi900_A = cfg.MCComponent(
+    name = "mDM100_mPhi900_A",
+    files = [
+    '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM100_mPhi900_fast.root',
+    ],
+    xSection = 6.78
+    )
+
+mDM10_mPhi100_A = cfg.MCComponent(
+    name = 'mDM10_mPhi100_A', 
+    files = [
+        '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM10_mPhi100_fast.root', 
+        ],
+    xSection = 4204.60
+    )
+
+mDM10_mPhi10_A = cfg.MCComponent(
+    name = 'mDM10_mPhi10_A', 
+    files = [
+        '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM10_mPhi10_fast.root', 
+        ],
+    xSection = 8027.16
+    )
+
+mDM10_mPhi1100_A = cfg.MCComponent(
+        name = 'mDM10_mPhi1100_A', 
+        files = [ 
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM10_mPhi1100_fast.root', 
+            ],
+        xSection = 3.98
+        )
+
+mDM10_mPhi1300_A = cfg.MCComponent(
+        name = 'mDM10_mPhi1300_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM10_mPhi1300_fast.root', 
+            ],
+        xSection = 2.12
+        )
+
+mDM10_mPhi300_A = cfg.MCComponent(
+        name = 'mDM10_mPhi300_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM10_mPhi300_fast.root', 
+            ],
+        xSection = 289.19
+        )
+
+mDM10_mPhi500_A = cfg.MCComponent(
+        name = 'mDM10_mPhi500_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM10_mPhi500_fast.root', 
+            ],
+        xSection = 60.54
+        )
+
+mDM10_mPhi50_A = cfg.MCComponent(
+        name = 'mDM10_mPhi50_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM10_mPhi50_fast.root', 
+            ],
+        xSection = 14519.95
+        )
+
+mDM10_mPhi700_A = cfg.MCComponent(
+        name = 'mDM10_mPhi700_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM10_mPhi700_fast.root', 
+            ],
+        xSection = 19.72
+        )
+
+mDM10_mPhi900_A = cfg.MCComponent(
+        name = 'mDM10_mPhi900_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM10_mPhi900_fast.root', 
+            ],
+        xSection = 8.24
+        )
+
+mDM200_mPhi100_A = cfg.MCComponent(
+        name = 'mDM200_mPhi100_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM200_mPhi100_fast.root', 
+            ],
+        xSection = 3.55
+        )
+
+mDM200_mPhi10_A = cfg.MCComponent(
+        name = 'mDM200_mPhi10_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM200_mPhi10_fast.root', 
+            ],
+        xSection = 3.37
+        )
+
+mDM200_mPhi1100_A = cfg.MCComponent(
+        name = 'mDM200_mPhi1100_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM200_mPhi1100_fast.root', 
+            ],
+        xSection = 2.49
+        )
+
+mDM200_mPhi1300_A = cfg.MCComponent(
+        name = 'mDM200_mPhi1300_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM200_mPhi1300_fast.root', 
+            ],
+        xSection = 1.42
+        )
+
+mDM200_mPhi300_A = cfg.MCComponent(
+        name = 'mDM200_mPhi300_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM200_mPhi300_fast.root', 
+            ],
+        xSection = 5.97
+        )
+
+mDM200_mPhi500_A = cfg.MCComponent(
+        name = 'mDM200_mPhi500_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM200_mPhi500_fast.root', 
+            ],
+        xSection = 13.00
+        )
+
+mDM200_mPhi50_A = cfg.MCComponent(
+        name = 'mDM200_mPhi50_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM200_mPhi50_fast.root', 
+            ],
+        xSection = 3.42
+        )
+
+mDM200_mPhi700_A = cfg.MCComponent(
+        name = 'mDM200_mPhi700_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM200_mPhi700_fast.root', 
+            ],
+        xSection = 8.52
+        )
+
+mDM200_mPhi900_A = cfg.MCComponent(
+        name = 'mDM200_mPhi900_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM200_mPhi900_fast.root', 
+            ],
+        xSection = 4.57
+        )
+
+mDM300_mPhi100_A = cfg.MCComponent(
+        name = 'mDM300_mPhi100_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM300_mPhi100_fast.root', 
+            ],
+        xSection = 0.81
+        )
+
+mDM300_mPhi10_A = cfg.MCComponent(
+        name = 'mDM300_mPhi10_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM300_mPhi10_fast.root', 
+            ],
+        xSection = 0.79
+        )
+
+mDM300_mPhi1100_A = cfg.MCComponent(
+        name = 'mDM300_mPhi1100_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM300_mPhi1100_fast.root', 
+            ],
+        xSection = 1.62
+        )
+
+mDM300_mPhi1300_A = cfg.MCComponent(
+        name = 'mDM300_mPhi1300_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM300_mPhi1300_fast.root', 
+            ],
+        xSection = 1.01
+        )
+
+mDM300_mPhi300_A = cfg.MCComponent(
+        name = 'mDM300_mPhi300_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM300_mPhi300_fast.root', 
+            ],
+        xSection = 1.00
+        )
+
+mDM300_mPhi500_A = cfg.MCComponent(
+        name = 'mDM300_mPhi500_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM300_mPhi500_fast.root', 
+            ],
+        xSection = 1.71
+        )
+
+mDM300_mPhi50_A = cfg.MCComponent(
+        name = 'mDM300_mPhi50_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM300_mPhi50_fast.root', 
+            ],
+        xSection = 0.79
+        )
+
+mDM300_mPhi700_A = cfg.MCComponent(
+        name = 'mDM300_mPhi700_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM300_mPhi700_fast.root', 
+            ],
+        xSection = 2.95
+        )
+
+mDM300_mPhi900_A = cfg.MCComponent(
+        name = 'mDM300_mPhi900_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM300_mPhi900_fast.root', 
+            ],
+        xSection = 2.47
+        )
+
+mDM400_mPhi100_A = cfg.MCComponent(
+        name = 'mDM400_mPhi100_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM400_mPhi100_fast.root', 
+            ],
+        xSection = 0.25
+        )
+
+mDM400_mPhi10_A = cfg.MCComponent(
+        name = 'mDM400_mPhi10_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM400_mPhi10_fast.root', 
+            ],
+        xSection = 0.25
+        )
+
+mDM400_mPhi1100_A = cfg.MCComponent(
+        name = 'mDM400_mPhi1100_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM400_mPhi1100_fast.root', 
+            ],
+        xSection = 0.88
+        )
+
+mDM400_mPhi1300_A = cfg.MCComponent(
+        name = 'mDM400_mPhi1300_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM400_mPhi1300_fast.root', 
+            ],
+        xSection = 0.65
+        )
+
+mDM400_mPhi300_A = cfg.MCComponent(
+        name = 'mDM400_mPhi300_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM400_mPhi300_fast.root', 
+            ],
+        xSection = 0.29
+        )
+
+mDM400_mPhi500_A = cfg.MCComponent(
+        name = 'mDM400_mPhi500_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM400_mPhi500_fast.root', 
+            ],
+        xSection = 0.38
+        )
+
+mDM400_mPhi50_A = cfg.MCComponent(
+        name = 'mDM400_mPhi50_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM400_mPhi50_fast.root', 
+            ],
+        xSection = 0.25
+        )
+
+mDM400_mPhi700_A = cfg.MCComponent(
+        name = 'mDM400_mPhi700_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM400_mPhi700_fast.root', 
+            ],
+        xSection = 0.62
+        )
+
+mDM400_mPhi900_A = cfg.MCComponent(
+        name = 'mDM400_mPhi900_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM400_mPhi900_fast.root', 
+            ],
+        xSection = 0.94
+        )
+
+mDM500_mPhi100_A = cfg.MCComponent(
+        name = 'mDM500_mPhi100_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM500_mPhi100_fast.root', 
+            ],
+        xSection = 9.63
+        )
+
+mDM500_mPhi10_A = cfg.MCComponent(
+        name = 'mDM500_mPhi10_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM500_mPhi10_fast.root', 
+            ],
+        xSection = 9.54
+        )
+
+mDM500_mPhi1100_A = cfg.MCComponent(
+        name = 'mDM500_mPhi1100_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM500_mPhi1100_fast.root', 
+            ],
+        xSection = 0.36
+        )
+
+mDM500_mPhi1300_A = cfg.MCComponent(
+        name = 'mDM500_mPhi1300_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM500_mPhi1300_fast.root', 
+            ],
+        xSection = 0.35
+        )
+
+mDM500_mPhi300_A = cfg.MCComponent(
+        name = 'mDM500_mPhi300_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM500_mPhi300_fast.root', 
+            ],
+        xSection = 0.10
+        )
+
+mDM500_mPhi500_A = cfg.MCComponent(
+        name = 'mDM500_mPhi500_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM500_mPhi500_fast.root', 
+            ],
+        xSection = 0.12
+        )
+
+mDM500_mPhi50_A = cfg.MCComponent(
+        name = 'mDM500_mPhi50_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM500_mPhi50_fast.root', 
+            ],
+        xSection = 9.56
+        )
+
+mDM500_mPhi700_A = cfg.MCComponent(
+        name = 'mDM500_mPhi700_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM500_mPhi700_fast.root', 
+            ],
+        xSection = 0.17
+        )
+
+mDM500_mPhi900_A = cfg.MCComponent(
+        name = 'mDM500_mPhi900_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM500_mPhi900_fast.root', 
+            ],
+        xSection = 0.26
+        )
+
+mDM50_mPhi100_A = cfg.MCComponent(
+        name = 'mDM50_mPhi100_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM50_mPhi100_fast.root', 
+            ],
+        xSection = 545.02
+        )
+
+mDM50_mPhi10_A = cfg.MCComponent(
+        name = 'mDM50_mPhi10_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM50_mPhi10_fast.root', 
+            ],
+        xSection = 205.67
+        )
+
+mDM50_mPhi1100_A = cfg.MCComponent(
+        name = 'mDM50_mPhi1100_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM50_mPhi1100_fast.root', 
+            ],
+        xSection = 3.76
+        )
+
+mDM50_mPhi1300_A = cfg.MCComponent(
+        name = 'mDM50_mPhi1300_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM50_mPhi1300_fast.root', 
+            ],
+        xSection = 2.01
+        )
+
+mDM50_mPhi300_A = cfg.MCComponent(
+        name = 'mDM50_mPhi300_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM50_mPhi300_fast.root', 
+            ],
+        xSection = 227.28
+        )
+
+mDM50_mPhi500_A = cfg.MCComponent(
+        name = 'mDM50_mPhi500_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM50_mPhi500_fast.root', 
+            ],
+        xSection = 53.82
+        )
+
+mDM50_mPhi50_A = cfg.MCComponent(
+        name = 'mDM50_mPhi50_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM50_mPhi50_fast.root', 
+            ],
+        xSection = 246.66
+        )
+
+mDM50_mPhi700_A = cfg.MCComponent(
+        name = 'mDM50_mPhi700_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM50_mPhi700_fast.root', 
+            ],
+        xSection = 18.18
+        )
+
+mDM50_mPhi900_A = cfg.MCComponent(
+        name = 'mDM50_mPhi900_A', 
+        files = [
+            '/afs/cern.ch/work/p/penning/public/fastsim/miniAOD_A_mDM50_mPhi900_fast.root', 
+            ],
+        xSection = 7.72
+        )
+
+dmTest = [
+mDM100_mPhi100_A,
+mDM100_mPhi10_A ,
+mDM100_mPhi1100_A ,
+mDM100_mPhi1300_A ,
+mDM100_mPhi300_A ,
+mDM100_mPhi500_A ,
+mDM100_mPhi50_A  ,
+mDM100_mPhi700_A ,
+mDM100_mPhi900_A ,
+#mDM10_mPhi100_A ,
+mDM10_mPhi10_A ,
+mDM10_mPhi1100_A ,
+mDM10_mPhi1300_A ,
+mDM10_mPhi300_A ,
+mDM10_mPhi500_A ,
+mDM10_mPhi50_A ,
+mDM10_mPhi700_A ,
+mDM10_mPhi900_A ,
+mDM200_mPhi100_A ,
+mDM200_mPhi10_A ,
+mDM200_mPhi1100_A ,
+mDM200_mPhi1300_A ,
+mDM200_mPhi300_A ,
+mDM200_mPhi500_A ,
+mDM200_mPhi50_A ,
+mDM200_mPhi700_A ,
+mDM200_mPhi900_A ,
+mDM300_mPhi100_A ,
+mDM300_mPhi10_A ,
+mDM300_mPhi1100_A ,
+mDM300_mPhi1300_A ,
+mDM300_mPhi300_A ,
+mDM300_mPhi500_A ,
+mDM300_mPhi50_A ,
+mDM300_mPhi700_A ,
+mDM300_mPhi900_A ,
+mDM400_mPhi100_A ,
+mDM400_mPhi10_A ,
+mDM400_mPhi1100_A ,
+mDM400_mPhi1300_A ,
+mDM400_mPhi300_A ,
+mDM400_mPhi500_A ,
+mDM400_mPhi50_A ,
+mDM400_mPhi700_A ,
+mDM400_mPhi900_A ,
+mDM500_mPhi100_A ,
+mDM500_mPhi10_A ,
+mDM500_mPhi1100_A ,
+mDM500_mPhi1300_A ,
+mDM500_mPhi300_A ,
+mDM500_mPhi500_A ,
+mDM500_mPhi50_A ,
+mDM500_mPhi700_A ,
+mDM500_mPhi900_A ,
+mDM50_mPhi100_A ,
+mDM50_mPhi10_A ,
+mDM50_mPhi1100_A ,
+mDM50_mPhi1300_A ,
+mDM50_mPhi300_A ,
+mDM50_mPhi500_A ,
+mDM50_mPhi50_A ,
+mDM50_mPhi700_A ,
+mDM50_mPhi900_A ,
+]
+
