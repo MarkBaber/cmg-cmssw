03e5e1f4b7d67ca778a7d7d094351fc65ef6ab3c
aelwood <adam.elwood09@imperial.ac.uk>
2015-03-26 02:12:52 +0100

    Added minDeltaR values back into trees

--- CMGTools/TTHAnalysis/python/analyzers/treeProducerSusyAlphaT.py
+++ CMGTools/TTHAnalysis/python/analyzers/treeProducerSusyAlphaT.py
@@ -102,9 +102,8 @@ susyAlphaT_collections.update({
     "cleanGenJets"         : NTupleCollection("genJet",     genParticleType, 10, help="Generated jets (cleaned)"),
 
     # dR jet lep for each lepton
-    # not necessary as muon and electron have jet DR in tree already
-    #"minDeltaRLepJet"  : NTupleCollection("minDeltaRLepJet", double, 50, help="Min deltaR between a lepton and all the jets"),
-    #"minDeltaRPhoJet"  : NTupleCollection("minDeltaRPhoJet", double, 50, help="Min deltaR between a photon and all the jets"),
+    "minDeltaRLepJet"  : NTupleCollection("minDeltaRLepJet", double, 50, help="Min deltaR between a lepton and all the jets"),
+    "minDeltaRPhoJet"  : NTupleCollection("minDeltaRPhoJet", double, 50, help="Min deltaR between a photon and all the jets"),
 })
 
             
