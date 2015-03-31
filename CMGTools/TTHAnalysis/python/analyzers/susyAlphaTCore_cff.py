95bb94dea56f7b47a671894a550db19d92d9dc58
aelwood <adam.elwood09@imperial.ac.uk>
2015-03-31 14:44:36 +0200

    Set default jets to be cleaned from photons as well as leptons

--- CMGTools/TTHAnalysis/python/analyzers/susyAlphaTCore_cff.py
+++ CMGTools/TTHAnalysis/python/analyzers/susyAlphaTCore_cff.py
@@ -57,6 +57,7 @@ jetAna.jetEta          = 5.
 jetAna.jetEtaCentral   = 3.
 jetAna.jetPt           = 40.
 jetAna.mcGT = "PHYS14_25_V2_LowPtHenningFix" #Henning's fix for spike in low PT JEC
+jetAna.alwaysCleanPhotons = True
 jetAna.cleanGenJetsFromPhoton = True
 
 # ttHJetMCAna.smearJets     = False
