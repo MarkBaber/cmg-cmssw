70bf919dfe49654e0664c788e273234d2b9305d4
aelwood <adam.elwood09@imperial.ac.uk>
2015-04-23 11:47:07 +0200

    Deleted extra line

--- PhysicsTools/Heppy/python/analyzers/gen/all.py
+++ PhysicsTools/Heppy/python/analyzers/gen/all.py
@@ -2,4 +2,3 @@ from PhysicsTools.Heppy.analyzers.gen.GeneratorAnalyzer import GeneratorAnalyzer
 from PhysicsTools.Heppy.analyzers.gen.GenHeavyFlavourAnalyzer import GenHeavyFlavourAnalyzer
 from PhysicsTools.Heppy.analyzers.gen.HiggsDecayModeAnalyzer import HiggsDecayModeAnalyzer
 from PhysicsTools.Heppy.analyzers.gen.PDFWeightsAnalyzer import PDFWeightsAnalyzer
-
